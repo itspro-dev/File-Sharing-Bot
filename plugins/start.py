73ab5d25b1e9b4f65a00857c83ff2d61de0e9870
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2021-09-03 16:54:12 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -69,15 +69,6 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
         return
-    else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
